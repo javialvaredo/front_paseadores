90ff370c57de3534b6babc87fe29bd42d27b2d22
Javier Alvaredo <javi.alvaredo@gmail.com>
2024-06-28 18:46:43 -0300

    cambio variable servidor

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-//export const SERVER_URL = 'https://javi-alvaredo.alwaysdata.net';
-export const SERVER_URL = 'http://localhost:3300';
+export const SERVER_URL = 'https://javi-alvaredo.alwaysdata.net';
+//export const SERVER_URL = 'http://localhost:3300';
